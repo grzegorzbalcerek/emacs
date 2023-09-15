c84824d5b1d7e31b406dd74c84f07c7d861f4cdb
Grzegorz Balcerek <gbalcerek72@gmail.com>
2023-09-15 15:56:37 +0200

    org.el

--- org.el
+++ org.el
@@ -2,6 +2,7 @@
 (defun org-mode-customizations()
   (interactive)
   (local-set-key [return] 'newline)
+  (local-set-key [C-S-return] 'duplicate-line)
   )
 
 (add-hook 'org-mode-hook 'org-mode-customizations)
