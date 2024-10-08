90bcb1b1be5878819a2971c43c4e9d0f3b69e1ba
Grzegorz Balcerek <gbalcerek72@gmail.com>
2024-10-08 19:48:19 +0200

    htmlxmlplus.el

--- htmlxmlplus.el
+++ htmlxmlplus.el
@@ -58,6 +58,32 @@
         (shell-command-on-region (point-min) (point-max) cmd t "*err*")
       (shell-command-on-region (point-min) (point-max) cmd))))
 
+(defun set-or-replace-term-or-span-class(lvl color)
+  "Find the beginning of a tag, skip it,
+   find the class attribute or add it,
+   remove any lvl attribute value if any,
+   add the lvl plus color attribute value if color is not nil"
+  (while (not (looking-at "<term\\|<span")) (backward-char))
+  (if (looking-at "<term\\|<span")
+      (progn
+        (forward-word)
+        (while (not (looking-at " class=\\|>")) (forward-char))
+        (if (looking-at ">")
+            (progn (insert " class=\"\"") (backward-char))
+          (forward-char 8))
+        (while (not (looking-at "['\"]"))
+          (cond
+           ((looking-at (concat lvl "[a-z][a-z][ '\"]"))
+            (delete-char 5))
+           ((looking-at "  ")
+            (delete-char 1))
+           (t (forward-char))))
+        (while (looking-back " ") (delete-char -1))
+        (if color
+            (progn
+              (if (not (looking-back "['\"]")) (insert " "))
+              (insert lvl color))))))
+
 (defun xmlhtml-modes-customizations()
   (interactive)
   (message "xmlhtml-modes-customizations")
@@ -73,3 +99,38 @@
 
 (add-hook 'mhtml-mode-hook 'xmlhtml-modes-customizations)
 (add-hook 'nxml-mode-hook 'xmlhtml-modes-customizations)
+
+(define-minor-mode piece-colors-minor-mode
+  "Toggle set piece colors minor mode. When mode is enabled, certain letter keys add piece-level classes"
+  nil
+  " PieceColors"
+  '(
+    ("a" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fa")))
+    ("b" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fb")))
+    ("c" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fc")))
+    ("d" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fd")))
+    ("e" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fe")))
+    ("f" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "ff")))
+    ("g" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fg")))
+    ("h" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fh")))
+    ("i" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fi")))
+    ("j" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fj")))
+    ("k" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fk")))
+    ("l" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fl")))
+    ("m" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fm")))
+    ("n" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fn")))
+    ("o" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fo")))
+    ("p" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fp")))
+    ("q" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fq")))
+    ("r" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fr")))
+    ("s" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fs")))
+    ("t" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "ft")))
+    ("v" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fv")))
+    ("w" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fw")))
+    ("x" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fx")))
+    ("y" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fy")))
+    ("z" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" "fz")))
+    (";" . (lambda()(interactive)(set-or-replace-term-or-span-class "pie" nil)))
+    ("`" . (lambda()(interactive)(piece-colors-minor-mode -1)))
+    ))
+(global-set-key (kbd "C-M-s-'") 'piece-colors-minor-mode)
